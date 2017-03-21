29b98a287acfbbbe92269b8e9217e200daabbf2c
Gavyn <gavyn.riebau@gmail.com>
2017-03-21 21:04:03 +0800

    Added command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,47 @@
 
 extern crate hex;
+extern crate clap;
 
 use std::io;
-use std::io::{Read, Write};
-
+use std::io::{Write, BufRead};
+use clap::{App, Arg};
 
 fn main() {
 
+    let matches = App::new("hex2ascii")
+        .author("Gavyn Riebau")
+        .version("0.1.0")
+        .about("Converts hex values to ascii")
+        .arg(
+            Arg::with_name("verbose")
+                .help("Include verbose output including warning messages written to stderr")
+                .long("verbose")
+                .short("v")
+        )
+        .get_matches();
+
     let mut stderr = io::stderr();
-    let mut stdin = io::stdin();
     let mut stdout = io::stdout();
-    let mut buffer : [u8; 2] = [0; 2];
+    let stdin = io::stdin();
 
-    loop {
-        let bytes_read = stdin.read(&mut buffer).unwrap();
+    let verbose = matches.is_present("verbose");
 
-        if bytes_read == 0 {
-            break;
-        }
+    for line in stdin.lock().lines() {
 
-        match hex::FromHex::from_hex(buffer) {
+        match hex::FromHex::from_hex(line.unwrap().into_bytes()) {
             Ok(buffer) => {
                 let unencoded = String::from_utf8(buffer).unwrap();
-                print!("{}", unencoded);
+                println!("{}", unencoded);
                 let _ = stdout.flush();
             },
             Err(e) => {
-                let _ = stderr.write_fmt(format_args!("Error was: {}", e));
-                let _ = stderr.flush();
+                if verbose {
+                    let _ = stderr.write_fmt(format_args!("Error: {}\n", e));
+                    let _ = stderr.flush();
+                }
             }
         }
 
     }
 }
+
